1b8eedc784bea5721c2b6c1629383e43164ce6ca
Yunpeng Niu <neilniuyunpeng@gmail.com>
2020-05-06 18:08:45 +0800

    Refactor

--- ping.py
+++ ping.py
@@ -50,40 +50,25 @@ def main():
 
     try:
         print('==================================')
+        endpoints = [
+            'https://graph.microsoft.com/v1.0/drive/root',
+            'https://graph.microsoft.com/v1.0/me/drive',
+            'https://graph.microsoft.com/v1.0/me/drive/root',
+            'https://graph.microsoft.com/v1.0/me/drive/root/children',
+            'https://graph.microsoft.com/v1.0/users',
+            'https://graph.microsoft.com/v1.0/me/messages',
+            'https://graph.microsoft.com/v1.0/me/mailFolders',
+            'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules',
+            'https://graph.microsoft.com/v1.0/me/outlook/masterCategories',
+            'https://api.powerbi.com/v1.0/myorg/apps'
+        ]
 
         # Calls all the different APIs.
-        if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root', headers=headers).status_code == 200:
-            num1 += 1
-            print("#{}: 1st call success".format(num1))
-        if req.get(r'https://graph.microsoft.com/v1.0/me/drive', headers=headers).status_code == 200:
-            num1 += 1
-            print("#{}: 2nd call success".format(num1))
-        if req.get(r'https://graph.microsoft.com/v1.0/drive/root', headers=headers).status_code == 200:
-            num1 += 1
-            print("#{}: 3rd call success".format(num1))
-        if req.get(r'https://graph.microsoft.com/v1.0/users ', headers=headers).status_code == 200:
-            num1 += 1
-            print("#{}: 4th call success".format(num1))
-        if req.get(r'https://graph.microsoft.com/v1.0/me/messages', headers=headers).status_code == 200:
-            num1 += 1
-            print("#{}: 5th call success".format(num1))
-        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules', headers=headers).status_code == 200:
-            num1 += 1
-            print("#{}: 6th call success".format(num1))
-        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messageRules', headers=headers).status_code == 200:
-            num1 += 1
-            print("#{}: 7th call success".format(num1))
-        if req.get(r'https://graph.microsoft.com/v1.0/me/drive/root/children', headers=headers).status_code == 200:
-            num1 += 1
-            print("#{}: 8th call success".format(num1))
-        if req.get(r'https://api.powerbi.com/v1.0/myorg/apps', headers=headers).status_code == 200:
-            num1 += 1
-            print("#{}: 9th call success".format(num1))
-        if req.get(r'https://graph.microsoft.com/v1.0/me/mailFolders', headers=headers).status_code == 200:
-            num1 += 1
-            print("#{}: 10th call success".format(num1))
-        if req.get(r'https://graph.microsoft.com/v1.0/me/outlook/masterCategories', headers=headers).status_code == 200:
-            num1 += 1
+        for endpoint in endpoints:
+            resp = req.get(endpoint, headers=headers)
+            if resp.status_code == 200:
+                num1 += 1
+                print("#{}: API call to {} success".format(num1, endpoint))
             print("#{}: 11th call success".format(num1))
         
         # Reports the timing.
